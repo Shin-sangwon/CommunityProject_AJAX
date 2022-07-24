c60e9e566cfc20a3f49f43be99d3530b362201fa
Shin-sangwon <dnjsml10@gmail.com>
2022-07-25 08:49:15 +0900

    게시물 출력

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -1,9 +1,22 @@
 package com.ll.exam.article;
 
 import com.ll.exam.Rq;
+import com.ll.exam.article.dto.ArticleDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArticleController {
     public void showList(Rq rq){
-        rq.appendBody("board list");
+        List<ArticleDto> articleDtoList = new ArrayList<>();
+
+        articleDtoList.add(new ArticleDto(3, "제목 3", "내용 3"));
+        articleDtoList.add(new ArticleDto(2, "제목 2", "내용 2"));
+        articleDtoList.add(new ArticleDto(1, "제목 1", "내용 1"));
+
+        rq.setAttr("articles", articleDtoList);
+
+        rq.view("usr/article/list");
+
     }
 }
